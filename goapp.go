1faaf9f6f4fd0aeac9a772e16e0bbf855c964b57
Alexys Lozada <alexyslozada@msn.com>
2016-09-10 14:25:38 -0500

    Funcionalidad de edición de páginas
    Se crea la funcionalidad de edición de páginas la cual permite
    editar una página existente o crear una nueva si es que no existe.

--- goapp.go
+++ goapp.go
@@ -38,7 +38,45 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	page, err := loadPage(title)
+	if err != nil {
+		page = &Page{Title: title}
+	}
+	fmt.Fprintf(w, `
+		<html>
+		<head>
+			<title>%s</title>
+		</head>
+		<body>
+			<h1>%s</h1>
+			<form method="POST" action="/save/%s">
+				<textarea name="body">%s</textarea>
+				<button>Guardar</button>
+			</form>
+		</body>
+		</html>
+	`, page.Title, page.Title, page.Title, page.Body)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	page := &Page{Title: title, Body: []byte(body)}
+	page.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Bienvenidos</h1>")
+}
+
 func main() {
+	http.HandleFunc("/", welcomeHandler)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	fmt.Println("Ingrese a http://localhost:8080/view/")
 	http.ListenAndServe(":8080", nil)
 }
